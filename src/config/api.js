6a47c4c8d3f241d069f4558417ea5ac222e77710
foushware <mr.fouad992@gmail.com>
2021-09-24 23:58:33 +0200

    Ready to deploy

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: `http://localhost:3000/`,
+  // baseURL: `http://localhost:3000/`,
+  baseURL: `https://mcqquestionstest.herokuapp.com/questions`,
   mode: 'no-cors',
 })
